98f55e37e5b2707b0c828c9c6cd80beaf7243160
fancn21th <fancn21th@gmail.com>
2024-04-16 14:42:20 +0800

    📦 NEW: Shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
+
+// https://www.youtube.com/watch?v=tSI98g3PDyE
+// https://ui.shadcn.com/docs/installation/next
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Flowise",
@@ -13,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
