39832c58fb58912cdc00d43b49668a5e1b031626
fancn21th <fancn21th@gmail.com>
2024-04-16 16:18:40 +0800

    📦 NEW: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { SiteHeader } from "@/components/site-header";
 
 // https://www.youtube.com/watch?v=tSI98g3PDyE
 // https://ui.shadcn.com/docs/installation/next
@@ -29,6 +30,7 @@ export default function RootLayout({
         )}
       >
         <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader />
           <main className="flex-1">{children}</main>
         </div>
       </body>
